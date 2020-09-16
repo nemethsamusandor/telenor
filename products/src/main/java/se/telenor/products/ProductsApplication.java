1120905937edda1f5a1f1c4d08ece674b36eeef5
nsa <extern.sandor.nemeth@porscheinformatik.com>
2020-09-16 17:50:28 +0200

    Initial commit with Liquibase

---
+++ products/src/main/java/se/telenor/products/ProductsApplication.java
@@ -0,0 +1,15 @@
+package se.telenor.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProductsApplication.class, args);
+    }
+
+}
